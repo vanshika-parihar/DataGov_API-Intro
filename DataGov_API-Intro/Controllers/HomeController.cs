7e68497d436d3f5805e9e02492439d3c2e08502f
Vanshika Kanwar Parihar <vparihar@usf.edu>
2021-04-12 17:22:55 -0400

    Add project files.

---
+++ DataGov_API-Intro/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using DataGov_API_Intro.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+
+namespace DataGov_API_Intro.Controllers
+{
+    public class HomeController : Controller
+    {
+        HttpClient httpClient;
+
+        static string BASE_URL = "https://api.usa.gov/crime/fbi/sapi/"; //"https://developer.nps.gov/api/v1/";
+        static string API_KEY = "g1mjvzQVDzMmHo0UQgfjLMgV4pzyazleokQOM5XS"; //Add your API key here inside ""
+
+        // fbi api//  https://api.usa.gov/crime/fbi/sapi/api/participation/national
+        public IActionResult Index()
+        {
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Add("X-Api-Key", API_KEY);
+            httpClient.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            string NATIONAL_PARK_API_PATH = BASE_URL + "api/participation/national?limit=20";
+            string resultsData = "";
+
+            Results results = null;
+
+            httpClient.BaseAddress = new Uri(NATIONAL_PARK_API_PATH);
+
+            try
+            {
+                HttpResponseMessage response = httpClient.GetAsync(NATIONAL_PARK_API_PATH).GetAwaiter().GetResult();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    resultsData = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                }
+
+                if (!resultsData.Equals(""))
+                {
+                    // JsonConvert is part of the NewtonSoft.Json Nuget package
+                    results = JsonConvert.DeserializeObject<Results>(resultsData);
+                }
+            }
+            catch (Exception e)
+            {
+                // This is a useful place to insert a breakpoint and observe the error message
+                Console.WriteLine(e.Message);
+            }
+
+            return View(results);
+        }
+    }
+}
+
+
---
+++ DataGov_API-Intro/Models/Results.cs
@@ -0,0 +1,28 @@
+﻿namespace DataGov_API_Intro.Models
+{
+    public class Results
+    {
+        public Result[] results { get; set; }
+    }
+    public class Result
+    {
+        public int data_year { get; set; }
+        public int population { get; set; }
+        public int total_agency_count { get; set; }
+        public int published_agency_count { get; set; }
+        public int active_agency_count { get; set; }
+        public int covered_agency_count { get; set; }
+        public int population_covered { get; set; }
+        public int agency_count_nibrs_submitting { get; set; }
+        public int agency_count_leoka_submitting { get; set; }
+        public int agency_count_pe_submitting { get; set; }
+        public int agency_count_srs_submitting { get; set; }
+        public int agency_count_asr_submitting { get; set; }
+        public int agency_count_hc_submitting { get; set; }
+        public int agency_count_supp_submitting { get; set; }
+        public int nibrs_population_covered { get; set; }
+        public int total_population { get; set; }
+        public float nibrs_population_percentage_covered { get; set; }
+        public string csv_header { get; set; }
+    }
+}
